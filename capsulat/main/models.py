2d59eed3fb2d4f9ad6476acdc117e02c2c165030
M4Y50N <pethersonreis@gmail.com>
2022-06-06 19:53:36 -0300

    DB and Models are configured

--- capsulat/main/models.py
+++ capsulat/main/models.py
@@ -1,3 +1,20 @@
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+
+
+class AlgoPFazer(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    AlgoPFazer = models.ForeignKey(AlgoPFazer, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+    complete = models.BooleanField()
+
+    def __str__(self):
+        return self.text
