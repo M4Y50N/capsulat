2827406ab9d79e68904f9cc40254f3a2f3690c13
M4Y50N <pethersonreis@gmail.com>
2022-07-11 13:42:21 -0300

    class, room and messagens was put in db

--- base/views.py
+++ base/views.py
@@ -1,18 +1,21 @@
-from multiprocessing import context
 from django.shortcuts import render
+from .models import Room
 
 
-rooms = [
-    {'id': 1, 'name': 'programmer'},
-    {'id': 2, 'name': 'Python'},
-    {'id': 3, 'name': 'Django'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Programmer'},
+#     {'id': 2, 'name': 'Python'},
+#     {'id': 3, 'name': 'Django'},
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
