0659283a2eef85a6e5a0f22940a1e594ee02951e
M4Y50N <pethersonreis@gmail.com>
2022-07-11 13:46:14 -0300

    message and classe added to admin site

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Room
+from .models import Classe, Message, Room
 
+admin.site.register(Classe)
 admin.site.register(Room)
+admin.site.register(Message)
