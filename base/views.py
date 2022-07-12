ed58dc3c6c9e501be83853cc9ab2d6712de473bf
M4Y50N <pethersonreis@gmail.com>
2022-07-12 14:54:36 -0300

    user login, logout and restrict has been added

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect, render
+from django.http import HttpResponse
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -15,6 +17,9 @@ from .forms import RoomForm
 
 def loginPage(request):
 
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method == 'POST':
         username = request.POST.get("username")
         password = request.POST.get("password")
@@ -29,9 +34,16 @@ def loginPage(request):
         if user is not None:
             login(request, user)
             return redirect('home')
+        else:
+            messages.error(request, "Usuário ou senha inválidos!")
 
     context = {}
     return render(request, 'base/login_register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 
 def home(request):
@@ -55,6 +67,7 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 
+@login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
     if request.method == 'POST':
@@ -67,6 +80,7 @@ def createRoom(request):
     return render(request, 'base/room_form.html', context)
 
 
+@login_required(login_url='login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
@@ -81,8 +95,10 @@ def updateRoom(request, pk):
     return render(request, 'base/room_form.html', context)
 
 
+@login_required(login_url='login')
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
+
     if request.method == 'POST':
         room.delete()
         return redirect('home')
