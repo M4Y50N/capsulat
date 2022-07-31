130fa0c3b68116e584271ba36b983e3a13fb9163
Pethe <pethersonreis@gmail.com>
2022-07-31 14:56:23 -0300

    update

--- base/urls.py
+++ base/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('profile/<str:pk>', views.userProfile, name="user-profile"),
     path('user-config/<str:pk>', views.userConfig, name="user-config"),
     path('room-join/<str:pk>', views.joinRoom, name="join"),
-    path('cryp/<str:pk>', views.nowCrypDecryp, name="cryp-decryp"),
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerPage, name="register"),
